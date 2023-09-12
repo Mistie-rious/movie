94680557eba8cb7a72ea4d3f9942bdb473692957
Mistie-rious <misturayahaya1@gmail.com>
2023-09-12 23:08:51 +0100

    adding more

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import logo from './assets/tv.png'
+import home from './assets/home.png'
+import show from './assets/TVshow.png'
+import projector from './assets/Projector.png'
+import calendar from './assets/Calendar.png'
+
 function Home() {
     
   const apiKey = '56b10ced2747113175093596cb0982d5'
@@ -24,12 +29,29 @@ function Home() {
         })
   return (
     <div>
-        <nav className='flex flex-col'>
+        <nav className='flex w-[226px] flex-col'>
         <div className='flex'>
             <img src={logo}></img>
             <span>Moviebox</span>
         </div>
+        <div className='flex'>
+            <img src={home}></img>
+            <span>Home</span>
+        </div>
+        <div className='flex'>
+            <img src={show}></img>
+            <span>TV Show</span>
+        </div>
+        <div className='flex'>
+            <img src={calendar}></img>
+            <span>Upcoming</span>
+        </div>
+        <div>Play movie tickets</div>
+        <div className='flex'>
+            <img src=''></img>
+        </div>
         </nav>
+        <div></div>
     </div>
   )
 }
